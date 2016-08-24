a1a38d4fed255431145b48458ed208147477af3a
zak <zak.j.holt@gmail.com>
2016-08-24 19:53:07 -0400

    made it with a function

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 var body = $("body");
-for (var i = 1; i < 101; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    body.append("<p>fizz buzz</p>");
-  } else if (i % 3 === 0) {
-    body.append("<p>fizz</p>");
-  } else if (i % 5 === 0) {
-    body.append("<p>buzz</p>");
-  } else {
-    body.append("<p>" + i + "</p>");
-  }
-}
+
+var fizzBuzz = function(num) {
+    for (i = 1; i <= num; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            body.append("<p>fizzbuzz</p>");
+        } else if (i % 3 === 0) {
+            body.append("<p>fizz</p>");
+        } else if (i % 5 === 0) {
+            body.append("<p>buzz</p>");
+        } else {
+            body.append("<p>" + i + "</p>");
+        }
+    }
+};
+
+
+var userNum;
+$(document).ready(function() {
+    userNum = parseInt(prompt("Choose a number!"));
+    if (userNum % 1 === 0) {
+        fizzBuzz(userNum);
+    }
+});
